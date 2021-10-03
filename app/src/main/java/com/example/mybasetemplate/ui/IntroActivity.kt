b9c2592eb3c0cb57c07bd69a128821e791a3936d
victory316 <csh153@gmail.com>
2021-10-03 15:06:47 +0900

    develop - Applying style

--- app/src/main/java/com/example/mybasetemplate/ui/IntroActivity.kt
+++ app/src/main/java/com/example/mybasetemplate/ui/IntroActivity.kt
@@ -4,9 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mybasetemplate.R
 
+/**
+ *  앱이 시작된 뒤로 사용 가능한 기능들을 나열하는 Activity
+ */
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        setupUi()
+        subscribeUi()
+    }
+
+    private fun setupUi() {
+        // TODO 신규로 구현한 기능들을 리스트 형으로 확인 가능하도록 구현.
+    }
+
+    private fun subscribeUi() {
+
     }
 }
