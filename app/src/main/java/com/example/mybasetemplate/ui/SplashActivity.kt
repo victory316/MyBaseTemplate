86a4044843b8805383949b843e6b9ff9fdd3b677
victory316 <csh153@gmail.com>
2021-10-03 12:53:37 +0900

    develop - New branch for development

---
+++ app/src/main/java/com/example/mybasetemplate/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mybasetemplate.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mybasetemplate.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
