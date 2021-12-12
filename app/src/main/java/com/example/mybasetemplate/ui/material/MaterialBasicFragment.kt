f5970975c0004611fc5e012ad1d86492d33e2228
victory316 <csh153@gmail.com>
2021-12-12 12:47:11 +0900

    develop - 일부 불필요 코드 제거, 최신 fab 적용 위한 준비

--- app/src/main/java/com/example/mybasetemplate/ui/material/MaterialBasicFragment.kt
+++ app/src/main/java/com/example/mybasetemplate/ui/material/MaterialBasicFragment.kt
@@ -10,21 +10,10 @@ import com.example.mybasetemplate.ext.showToast
 import com.example.mybasetemplate.presentation.MaterialViewModel
 import com.google.android.material.datepicker.MaterialDatePicker
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
- * A simple [Fragment] subclass.
- * Use the [MaterialBasicFragment.newInstance] factory method to
- * create an instance of this fragment.
+ *  기본적인 컴포넌트 사용 예를 보이는 Fragment
  */
 class MaterialBasicFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     lateinit var binding: FragmentMaterialBasicBinding
     lateinit var materialViewModel: MaterialViewModel
 
@@ -41,12 +30,6 @@ class MaterialBasicFragment : Fragment() {
                 )
             )
         }
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
     }
 
     private fun setupUi() {
@@ -83,40 +66,25 @@ class MaterialBasicFragment : Fragment() {
         }
     }
 
-    private fun subscrbeUi() {
+    private fun subscribeUi() {
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding =
             FragmentMaterialBasicBinding.inflate(inflater, container, false)
 
         setupUi()
-        subscrbeUi()
+        subscribeUi()
 
-        // Inflate the layout for this fragment
         return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MaterialBasicFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MaterialBasicFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = MaterialBasicFragment()
     }
 }
