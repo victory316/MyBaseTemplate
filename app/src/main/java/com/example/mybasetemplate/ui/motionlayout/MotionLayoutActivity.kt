dbd6ee1a34a167ab23cbcbed65b6c3d2677a5445
victory316 <csh153@gmail.com>
2022-02-20 21:13:47 +0900

    기타 ViewModel 및 Activity 추가

--- app/src/main/java/com/example/mybasetemplate/ui/motionlayout/MotionLayoutActivity.kt
+++ app/src/main/java/com/example/mybasetemplate/ui/motionlayout/MotionLayoutActivity.kt
@@ -3,10 +3,33 @@ package com.example.mybasetemplate.ui.motionlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mybasetemplate.R
+import com.example.mybasetemplate.databinding.ActivityMotionLayoutBinding
+import com.example.mybasetemplate.presentation.MaterialViewModel
+import com.example.mybasetemplate.presentation.MotionLayoutViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MotionLayoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMotionLayoutBinding
+
+    @Inject
+    lateinit var motionLayoutViewModel: MotionLayoutViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_layout)
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding = ActivityMotionLayoutBinding.inflate(layoutInflater)
+
+        with(binding) {
+            viewModel = motionLayoutViewModel
+        }
+
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/mybasetemplate/ui/motionlayout/MotionTransitionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mybasetemplate.ui.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mybasetemplate.R
+
+class MotionTransitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_transition)
+    }
+}
