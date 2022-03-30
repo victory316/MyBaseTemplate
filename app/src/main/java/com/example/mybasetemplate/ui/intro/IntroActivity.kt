e2aeca240134c216c4e5c4a794c20b52d51deda4
SH CHoi <csh153@gmail.com>
2022-03-30 14:52:50 +0900

    Final line 추가, Simple fragment 추가

--- app/src/main/java/com/example/mybasetemplate/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/example/mybasetemplate/ui/intro/IntroActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mybasetemplate.ui.intro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.mybasetemplate.base.BaseActivity
