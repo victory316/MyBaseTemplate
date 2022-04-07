e75560b4a21dcce33ecc189b90ae6b46ca1d8c5e
victory316 <csh153@gmail.com>
2022-04-07 16:48:10 +0900

    Compose codelab 시작

---
+++ app/src/main/java/com/example/mybasetemplate/ui/compose/only_compose/ComposeCodelabActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mybasetemplate.ui.compose.only_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class ComposeCodelabActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello!")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Choi")
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
